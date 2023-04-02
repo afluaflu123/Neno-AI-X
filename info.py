0b755cda32559e104375376568bd9042ed99d604
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-04-02 19:39:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -66,6 +66,7 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+LOGIN_CHANNEL = int(login_channel) if login_channel and id_pattern.search(login_channel) else None
 
 # This is required for the plugins involving the file system.
 TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
