5b9643883987e1f37277b93f978bbf9743a40055
HacKer Jr ~ TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2023-04-02 21:25:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,8 +5,8 @@ import re
 import ast
 import math
 import random
+import datetime
 lock = asyncio.Lock()
-
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
 import pyrogram
