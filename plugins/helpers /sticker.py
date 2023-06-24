281658800449b8b3c7341ea7bcad201d556079a4
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-06-24 23:31:54 +0530

    Update sticker.py

--- plugins/helpers /sticker.py
+++ plugins/helpers /sticker.py
@@ -6,4 +6,4 @@ async def stickerid(bot, message):
     if message.reply_to_message.sticker:
        await message.reply(f"**Sticker ID is**  \n `{message.reply_to_message.sticker.file_id}` \n \n ** Unique ID is ** \n\n`{message.reply_to_message.sticker.file_unique_id}`", quote=True)
     else: 
-       await message.reply("Oops !! Not a sticker file")￼Enter
+       await message.reply("Oops !! Not a sticker file")
