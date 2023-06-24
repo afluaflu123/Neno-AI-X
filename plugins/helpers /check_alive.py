7f14f545585e8a5dfbf11ee6853d1530421716c1
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-06-24 19:10:56 +0530

    Update check_alive.py

--- plugins/helpers /check_alive.py
+++ plugins/helpers /check_alive.py
@@ -3,6 +3,7 @@ import random
 from pyrogram import Client, filters
 
 CMD = ["/", "."]
+BOT_START_TIME = time.time()
 
 @Client.on_message(filters.command("ping", CMD))
 async def ping(_, message):
@@ -10,4 +11,6 @@ async def ping(_, message):
     rm = await message.reply_text("...........")
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
-    await rm.edit(f"Pɪɴɢ🔥!\n{time_taken_s:.3f} ms")
+    uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - BOT_START_TIME))
+    await rm.edit(f"Pɪɴɢ🔥!\n{time_taken_s:.3f} ms"\n
+                  f"➪ 𝖡𝗈𝗍 𝖴𝗉𝗍𝗂𝗆𝖾: {uptime}")
