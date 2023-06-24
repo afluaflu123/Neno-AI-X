45ee49f7d15c642e26ae4b597781710be126e0cd
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-06-24 19:18:35 +0530

    Update check_alive.py

--- plugins/helpers /check_alive.py
+++ plugins/helpers /check_alive.py
@@ -12,5 +12,5 @@ async def ping(_, message):
     end_t = time.time()
     time_taken_s = (end_t - start_t) * 1000
     uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - BOT_START_TIME))
-    await rm.edit(f"Pɪɴɢ🔥!\n{time_taken_s:.3f} ms"\n
-                  f"➪ 𝖡𝗈𝗍 𝖴𝗉𝗍𝗂𝗆𝖾: {uptime}")
+    await rm.edit(f"Pɪɴɢ🔥!\n{time_taken_s:.3f} ms\n➪ 𝖡𝗈𝗍 𝖴𝗉𝗍𝗂𝗆𝖾: {uptime}")
+                  
